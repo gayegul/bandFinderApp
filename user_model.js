048a0581e2ccd55e8a77c9453e309e24d077b61b
Jacob Barnett <mrjbarnett@gmail.com>
2015-02-22 17:52:08 -0800

    router and user schema

---
+++ user_model.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+	basic: {
+		email: String,
+		password: String
+	}
+	userName:	String,
+	firstName: String,
+	location: String,
+	instruments: String,
+	bio: String,
+	likedUsers:[String],
+	unlikedUsers:[String]
+}),
+
+module.exports = mongoose.model('User', userSchema);
