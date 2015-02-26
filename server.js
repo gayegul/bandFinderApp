a3f77896163342507af4a1613527651fcbd5a272
Gaye Bulut <gayegul@gmail.com>
2015-02-26 15:08:54 -0800

    move connection module

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 'use strict';
 
+var pg = require('pg');
+
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  var query = client.query('SELECT * FROM your_table');
+
+  query.on('row', function(row) {
+    console.log(JSON.stringify(row));
+  });
+});
+
 var starter = function(testing, done) {
 	var express = require('express');
 	var db = require('./lib/db')(testing);
