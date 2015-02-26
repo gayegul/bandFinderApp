254ab4b7d15143c3af6ed32ad65919880e6d0125
Gaye Bulut <gayegul@gmail.com>
2015-02-26 00:21:28 -0800

    testing complete

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
 'use strict';
 
-var express = require('express');
-var db = require('./lib/db');
-var passport = require('passport');
+var starter = function(testing, done) {
+	var express = require('express');
+	var db = require('./lib/db')(testing);
+	var passport = require('passport');
 
-var app = express();
-app.set('appSecret', process.env.SECRET || 'changethis!');
-app.use(passport.initialize());
-require('./lib/passport')(passport, db);
+	var app = express();
+	app.set('appSecret', process.env.SECRET || 'changethis!');
+	app.use(passport.initialize());
+	require('./lib/passport')(passport, db);
 
-var userRouter = express.Router();
+	var userRouter = express.Router();
 
-require('./lib/router')(db, userRouter, passport, app.get('appSecret'));
+	require('./lib/router')(db, userRouter, passport, app.get('appSecret'));
+	app.use('/api', userRouter);
 
-app.use('/api', userRouter);
-
-db.start(function() {
-	app.listen(process.env.PORT || 3434, function() {
-		console.log('Server listening on port ' + (process.env.PORT || 3434));
+	db.start(function() {
+		app.listen(process.env.PORT || 3434, function() {
+			console.log('Server listening on port ' + (process.env.PORT || 3434));
+			if(done) {
+				done(app);
+			}
+		});
 	});
-});
+};
 
+if(!module.parent) {
+	//running as main
+	starter(false);
+} else {
+	module.exports = starter;
+}
